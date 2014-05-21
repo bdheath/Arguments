87ff445deb7eeb8c1bda7e7b731b171118f7b64a
Brad Heath <brad.heath@gmail.com>
2014-05-21 14:26:52 -0400

    Update arguments_courts.py

--- arguments_courts.py
+++ arguments_courts.py
@@ -1,7 +1,7 @@
 courts_list = [
 	["First Circuit", "1st Cir.", "U.S. Court of Appeals for the First Circuit"], 
 	["Second Circuit", "2d Cir.", "U.S. Court of Appeals for the Second Circuit"],
-	["Third Circuit", "3rd Cir.", "U.S. Court of Appeals for the Third Circuit"], 
+	["Third Circuit", "3d Cir.", "U.S. Court of Appeals for the Third Circuit"], 
 	["Fourth Circuit", "4th Cir.", "U.S. Court of Appeals for the Fourth Circuit"], 
 	["Fifth Circuit", "5th Cir.", "U.S. Court of Appeals for the Fifth Circuit"], 
 	["Sixth Circuit", "6th Cir.", "U.S. Court of Appeals for the Sixth Circuit"], 
