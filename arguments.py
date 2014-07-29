d0486eeeeb5b2110d54ae9a9a6261e7d89136869
bdheath <brad.heath@gmail.com>
2014-07-29 14:06:31 -0400

    Scheduled arguments
    Updating to scrape scheduled arguments as well as those that have taken
    place

--- arguments.py
+++ arguments.py
@@ -5,6 +5,14 @@
 # Capable of multithreading scrapes, 
 #
 # TODO
+# - Update social shares for arguments that will be happening TODAY
+# - Update 9th Cir. scrapes so the model knows what to do if it finds a case
+#   that moves from the schedule to being actually argued
+# 		- Determine whether we already have the case (query method)
+# 		- Load the case
+#		- Change its status and assign media
+#		- Re-save the case (requires new save)
+# - Figure out which arguments are prominent, give them special status
 # - Enhance logging
 # - Attempt to standardize/modularize more of the scrape code
 
@@ -21,6 +29,7 @@ import MySQLdb
 import re
 import feedparser
 import multiprocessing
+import mechanize
 
 # Import my libraries
 from arguments_log import argumentLog
@@ -65,6 +74,7 @@ class court:
 # Methods for reading and writing arguments from the database
 class argument:
 
+	# Attributes and default values
 	_uid = ''
 	_caption = ''
 	_docket_no = ''
@@ -81,10 +91,14 @@ class argument:
 	_duration = 0
 	_judges = ''
 	_court_id = 0
+	_status = 'argued'
+	_added = ''
+	_date_scheduled = ''
 
 	def __init__(self, docket_no = '', caption = '', case_url = '', \
 		media_url = '', media_type = '', media_size = 0, counsel = '', \
-		issues='', judges = '', argued='', duration = 0, court_id = 0, uid=None):
+		issues='', judges = '', argued='', duration = 0, court_id = 0, uid=None,
+		status = 'argued', date_scheduled = '' ):
 		self._docket_no = docket_no
 		self._caption = caption
 		self._case_url = case_url
@@ -100,10 +114,16 @@ class argument:
 		self._argued = argued
 		self._duration = duration
 		self._court_id = court_id
+		self._status = status
+		self._date_scheduled = date_scheduled
 		self.checkDB()
 		if uid == None:
-			self._uid = str(self._court_id) + '__' + self._docket_no.replace(' ','') \
-				+ '__' + str(self._argued).replace(' ','_')
+			if self._argued == '':
+				self._uid = str(self._court_id) + '__' + self._docket_no.replace(' ','') \
+					+ '__' + str(self._date_scheduled).replace(' ','_')
+			else:
+				self._uid = str(self._court_id) + '__' + self._docket_no.replace(' ','') \
+					+ '__' + str(self._argued).replace(' ','_')
 		return
 		
 	def exists(self):
@@ -138,33 +158,106 @@ class argument:
 		self._media_type = h['media_type']
 		self._original_media_url = h['original_media_url']
 		self._original_media_type = h['original_media_type']
-		
+		self._status = h['status']
+		self._issues = h['issues']
+		self._date_scheduled = h['date_scheduled']
+		self._added = h['added']
+		self._case_url = h['case_url']
+		self._argued = h['argued'],
+		self._issues = h['issues'],
+		self._judges = h['judges'],
+		self._original_media_size = h['original_media_size']
+		self._caption = h['caption']
+		self._docket_no = h['docket_no']
+
+	def update(self, media_type = None, media_url = None, argued = None, added = None,
+		original_media_url = None, original_media_type = None, status = None):
+
+		if media_type != None and media_type <> self._media_type:
+			self._media_type = media_type
+		if media_url != None and media_url <> self._media_url:
+			self._media_url = media_url
+		if argued != None and argued <> self._argued:
+			self._argued = argued
+		if original_media_url != None and original_media_url <> self._original_media_url:
+			self._original_media_url = original_media_url
+		if original_media_type != None and original_media_type <> self._original_media_type:
+			self._original_media_type = original_media_type
+		if argued != None and argued <> self._argued:
+			self._argued = argued
+		if status != None and status <> self._status:
+			# Also handle social shares here when the status changes
+			if self._status == 'scheduled' and status == 'argued':
+				if settings.tweetnew:
+					share.tweet(self)
+			self._status = status
+		return
+	
+	def save(self):
+		# Save an already existing record
+		if self._uid == 0:
+			print "  # EXCEPTION: You cannot update a case without a valid UID"
+		else:
+			db.execute(""" UPDATE """ + settings.dbname + """.""" + settings.dbtable + """
+				SET docket_no = %s,
+					caption = %s,
+					case_url = %s,
+					media_url = %s,
+					media_type = %s, 
+					media_size = %s,
+					counsel = %s,
+					issues = %s,
+					judges = %s,
+					argued = %s, 
+					duration = %s,
+					court_id = %s,
+					added = %s,
+					original_media_type = %s,
+					original_media_size = %s,
+					original_media_url = %s,
+					status = %s,
+					date_scheduled = %s
+				WHERE uid = %s """,
+				(self._docket_no, self._caption, self._case_url, self._media_url,
+				self._media_type, self._media_size, self._counsel, self._issues,
+				self._judges, self._argued, self._duration, self._court_id,
+				self._added, self._original_media_type, self._original_media_size,
+				self._original_media_url, self._status, self._date_scheduled,
+				self._uid, ))
+		return
+	
 	def write(self):
 		if self._court_id == 0:
 			print '  # EXCEPTION: You cannot write to an unspecified court '
 		else:
+			# Handle the optional stuff - media conversion, social shares, etc. 
 			if settings.downloadandconvert == True:
 				filename = utils.localFileName(prefix=self._court_id, url=self._media_url)
 				f = utils.convertFile(self._media_url, filename)
 			if settings.tweetnew:
 				share.tweet(self)
-			if self._media_type == '':
-				self._media_type = self._media_url[-3:].lower()
-			u = argumentUtils()
-			if self._media_size == 0:
-				self._media_size = u.getRemoteSize(self._media_url)
+				
+			# Clean up anything that might have been missed in passing data to the argument
+			if self._status == 'argued':
+				if self._media_type == '':
+					self._media_type = self._media_url[-3:].lower()
+				u = argumentUtils()
+				if self._media_size == 0:
+					self._media_size = u.getRemoteSize(self._media_url)
+				
 			print '   - NEW: ' + self._caption
 			db.execute(""" REPLACE INTO """ + settings.dbname + """.""" + settings.dbtable + """
 					(uid, docket_no, caption, case_url, media_url, media_type, media_size, counsel, issues, judges, argued, duration, court_id, added,
-					original_media_type, original_media_size, original_media_url) 
+					original_media_type, original_media_size, original_media_url,
+					status, date_scheduled) 
 					VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, NOW(),
-					%s, %s, %s) """,
+					%s, %s, %s, %s, %s) """,
 					(self._uid, self._docket_no, self._caption, self._case_url,
 					self._media_url, self._media_type, self._media_size, 
 					self._counsel, self._issues, self._judges, self._argued,
 					self._duration, self._court_id, 
 					self._original_media_type, self._original_media_size,
-					self._original_media_url, ))
+					self._original_media_url, self._status, self._date_scheduled ))
 	
 	def checkDB(self):
 		if str(type(db)) == "<class 'MySQLdb.cursors.DictCursor'>":
@@ -192,6 +285,13 @@ class argument:
 							duration INT,
 							modified TIMESTAMP,
 							added DATETIME,
+							status VARCHAR(255),
+							date_scheduled DATETIME,
+							date_schedule_added DATETIME,
+							KEY date_scheduled_idx(date_scheduled),
+							KEY status_idx(status),
+							KEY status_argued_idx(status, argued),
+							KEY status_argued_court_id_idx(status,argued,court_id),
 							KEY docket_no_idx(docket_no),
 							KEY added_idx(added),
 							KEY modified_idx(modified),
@@ -242,7 +342,7 @@ class argument:
 		return
 
 
-# Utilities class
+# Utility class for social tools
 class argumentShare:
 	
 	twitterAPI = ''
@@ -284,9 +384,44 @@ class argumentShare:
 	def fb(self, arg):
 		return
 	
-		
+# Utility class for querying and altering arguments		
 class argumentUtils:
 
+	def query(self, court_id = 0, docket_no = '', caption = '', issues = '', status = ''):
+		results = []
+		
+		if court_id == 0 and docket_no == '' and caption == '' and issues == '':
+			results = False
+		else:
+			terms = []
+			sql = """ SELECT uid, court_id FROM """ + settings.dbname + """.""" + settings.dbtable \
+				+ """ WHERE """
+			if status <> '':
+				terms.append(' status = "' + status + '" ')
+			if court_id <> 0:
+				terms.append(" court_id = " + str(court_id))
+			if docket_no <> '':
+				terms.append(' docket_no = "' + docket_no + '" ')
+			if caption <> '':
+				terms.append(' caption = "' + caption + '" ')
+
+			i = 0
+			for t in terms:
+				sql += t
+				if i < len(terms) - 1:
+					sql += " AND " 
+				i += 1
+			db.execute(sql)
+			h = db.fetchall()
+			
+			# Return a list of arguments
+			for c in h:
+				arg = argument()
+				arg.load(c['uid'])
+				results.append(arg)
+		
+		return results
+				
 	def convertMostRecent(self):
 		db.execute(""" SELECT uid, court_id, media_url FROM """ + settings.dbname + """.""" + settings.dbtable + """
 					WHERE media_type <> 'mp3' 
@@ -422,6 +557,7 @@ class argumentUtils:
 	
 
 # HERE ARE THE ROUTINES THAT SCRAPE INDIVIDUAL COURT SITES
+# ----------------------------------------------------------------------
 
 def scrape_1st():
 	print '-> Updating 1st Cir. data'
@@ -646,7 +782,8 @@ def scrape_9th():
 	URL = 'http://www.ca9.uscourts.gov/media/?z=2'
 	DT = re.compile('(\d\d)/(\d\d)/(\d\d\d\d)')
 	
-	try:
+	#try:
+	if 0 == 0:
 		html = urllib2.urlopen(URL).read()
 		s = BeautifulSoup(html)
 		tables = s.find_all('table')
@@ -667,15 +804,29 @@ def scrape_9th():
 						+ docket_no + '.wma'
 					media_type = 'wma'
 					if utils.checkValidMediaUrl(media_url):
-						arg = argument(docket_no = docket_no, court_id = court_id, 
-							media_type = media_type, media_url = media_url, 
-							argued = argued, judges = judges, caption = caption)
-						if not arg.exists():
-							arg.write()
+					
+						# See if we already have this in the database
+						# If so, load it and update
+						# If not, create
+						q = utils.query(court_id = court_id, docket_no = docket_no)
+						if q:
+							if len(q) == 1:
+								arg = q[0]
+								arg.update(media_type = media_type, 
+									media_url = media_url,
+									argued = argued,
+									status = 'argued')
+								arg.save()
+						else:
+							arg = argument(docket_no = docket_no, court_id = court_id, 
+								media_type = media_type, media_url = media_url, 
+								argued = argued, judges = judges, caption = caption)
+							if not arg.exists():
+								arg.write()
 			i += 1
-	except:
-		err = str(sys.exc_info()[0]) + ' -> ' + str(sys.exc_info()[1])
-		log.log('ERROR', err)	
+	#except:
+	#	err = str(sys.exc_info()[0]) + ' -> ' + str(sys.exc_info()[1])
+	#	log.log('ERROR', err)	
 	return
 
 def scrape_dc():
@@ -770,6 +921,50 @@ def scrape_fed():
 	
 def scrape_st_ohio():
 	return
+
+def scrape_calendar_9th():
+	# Scrape the argument calendar for the 9th Circuit
+	court_id = utils.getCourt('9th Cir.')
+	
+	br = mechanize.Browser()
+	startpage = 'http://www.ca9.uscourts.gov/calendar/'
+	TABLEPATTERN = re.compile('<table class="main_table">.*?<th>(\d\d\d\d)-(\d\d)-(\d\d)\&nbsp;(.*?)<\/table><br\/>', re.IGNORECASE)
+	CASEPATTERN = re.compile('<tr class=".*?content_row">.*?<a.*?>(.*?)</a>.*?<strong>(.*?)<\/strong> - (.*?)<span.*?<td.*?>(.*?)<\/td>', re.IGNORECASE)
+	
+	br.open(startpage)
+	links = br.links(url_regex = 'calendar/view.php')
+	newlinks = []
+	for l in links:
+		newlinks.append(l)
+	
+	for l in newlinks:
+		# open argument page
+		br.open(l.url)
+		
+		html = br.response().read()
+		html = re.sub('[\n\r]',' ', html)
+
+		for m in re.finditer(TABLEPATTERN, html):
+			date_scheduled = m.group(1) + '-' + m.group(2) + '-' + m.group(3)
+			#if date_scheduled > str(date.today()):
+			if date_scheduled > str(date.today()):
+
+				casehtml = m.group(4)
+				for c in re.finditer(CASEPATTERN, casehtml):
+					
+					docket_no = c.group(1)
+					caption = c.group(2)
+					issues = c.group(4) + ': ' + c.group(3)
+					status = 'scheduled'
+				
+					arg = argument(docket_no = docket_no, caption = caption,
+						issues = issues, status = status, court_id = court_id,
+						date_scheduled = date_scheduled )
+					if not arg.exists():
+						arg.write()
+			
+	
+	return
 	
 utils = argumentUtils()
 share = argumentShare()
@@ -784,6 +979,8 @@ if __name__ == '__main__':
 	# Now execute the scrapes
 	scrapes = [scrape_1st, scrape_3rd, scrape_4th, scrape_5th, scrape_6th,
 		scrape_7th, scrape_8th, scrape_9th, scrape_dc, scrape_fed, scrape_scotus,]
+
+	scrapes = [scrape_calendar_9th, scrape_9th, ]
 	
 	if settings.multiprocess:
 		pool = multiprocessing.Pool(processes=settings.maxprocesses)
--- arguments_convert.py
+++ arguments_convert.py
@@ -2,9 +2,9 @@ import MySQLdb
 import sys
 from arguments import argument
 from arguments import argumentUtils
-
 from arguments_settings import argumentSettings
-settings = argumentSettings()
 
+settings = argumentSettings()
 utils = argumentUtils()
+
 utils.convertMostRecent()
